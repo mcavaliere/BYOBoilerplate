8a6d1924df46231958a3047236d54bcec4d5bea3
Mike Cavaliere <mike@cavaliere.org>
2018-10-01 13:44:09 -0400

    Adds test coverage for Generator base getters

--- __tests__/generator.test.js
+++ __tests__/generator.test.js
@@ -1,5 +1,67 @@
 import Generator from '../lib/generator';
+import fs from 'fs-extra';
+import path from 'path';
 
-it('creates an instance', () => {
-    const g = new Generator('testType', {}, {});
+const coreConfig = JSON.parse(
+    fs.readFileSync('./byobconfig.json')
+);
+
+describe('class Generator', () => {
+    let g = null;
+
+    beforeEach(() => {
+        g = new Generator('view', coreConfig.generators.view, coreConfig);
+    });
+
+    it('creates an instance', () => {
+        expect(g).not.toBeNull();
+        expect(g.type).toBeDefined();
+        expect(g.config).toBeDefined();
+        expect(g.coreConfig).toBeDefined();
+        expect(g.templates).toBeDefined();
+        expect(g.engine).toBeDefined();
+    });
+
+    describe('templatesDir()', () => {
+        it('returns templates/view', () => {
+            expect(g.templatesDir).toBe(
+                path.join(
+                    process.cwd(),
+                    'templates',
+                    'view'
+                )
+            );
+        });
+
+    });
+
+    describe('templateFilenames', () => {
+        it('returns this generator\'s template filenames', () => {
+            expect(g.templateFilenames).toEqual(
+                expect.arrayContaining([
+                    'view.js.liquid',
+                    'index.js.liquid',
+                    'styles.js.liquid'
+                ])
+            );
+        });
+    });
+
+
+    describe('outputDir', () => {
+        it('returns the correct output directory for the generator', () => {
+            expect(g.outputDir('views')).toBe(
+                path.join(
+                    process.cwd(),
+                    'src',
+                    'views'
+                )
+            )
+        });
+    });
+
+    afterEach(() => {
+        g = null;
+    });
 });
+
